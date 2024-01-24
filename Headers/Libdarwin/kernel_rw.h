59b5f2f9c610a5bf20f6c0693a6d40cca9d3f54b
Rayyan Khan <145166752+cxdxn1@users.noreply.github.com>
2024-01-24 17:23:39 +0000

    added libdarwin + crate headers and dependencies

---
+++ Headers/Libdarwin/kernel_rw.h
@@ -0,0 +1,8 @@
+// Header file for kernel_rw.h
+
+#ifndef KERNEL_RW_H
+#define KERNEL_RW_H
+
+
+#endif // KERNEL_RW_H
+
---
+++ Headers/Libdarwin/mach.h
@@ -0,0 +1,8 @@
+// Header file for mach.h
+
+#ifndef MACH_H
+#define MACH_H
+
+
+#endif // MACH_H
+
